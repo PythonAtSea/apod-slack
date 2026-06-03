8477cda6ff7fe4da202166344d20fbbaae771be8
PythonAtSea <HudsonGreen27@gmail.com>
2026-06-03 16:35:43 -0400

    feat: basic APOD fetching

--- index.js
+++ index.js
@@ -8,22 +8,54 @@ const app = new App({
   socketMode: true,
 });
 
-function getApodMessage() {
+async function getApodMessage() {
+  let imageUrl = "https://picsum.photos/1024";
+  let hdUrl = "https://picsum.photos/1920";
+  let title = "Contact @pythonatsea if you see this";
+  try {
+    const response = await fetch(
+      `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}`,
+    );
+    const data = await response.json();
+    imageUrl = data.url;
+    hdUrl = data.hdurl;
+    title = data.title;
+  } catch (error) {
+    return {
+      blocks: [{ type: "text", text: error }],
+    };
+  }
   return {
     blocks: [
       {
         type: "header",
         text: {
           type: "plain_text",
-          text: "title",
+          text: title,
           emoji: true,
         },
         level: 1,
       },
       {
         type: "image",
-        image_url: "https://picsum.photos/1024",
+        image_url: imageUrl,
         alt_text: "",
+      },
+      {
+        type: "actions",
+        elements: [
+          {
+            type: "button",
+            style: "primary",
+            text: {
+              type: "plain_text",
+              text: "Full HD Image :external-link:",
+              emoji: true,
+            },
+            url: hdUrl,
+            action_id: "url",
+          },
+        ],
       },
     ],
   };
@@ -31,11 +63,15 @@ function getApodMessage() {
 
 app.command("/apod", async ({ ack, respond }) => {
   await ack();
-  await respond(getApodMessage());
+  await respond(await getApodMessage());
 });
 
 app.event("app_mention", async ({ say }) => {
-  await say(getApodMessage());
+  await say(await getApodMessage());
+});
+
+app.action("url", async ({ ack, respond }) => {
+  await ack();
 });
 
 (async () => {
